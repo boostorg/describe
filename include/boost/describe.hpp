81613164ae948be33e1e7cc1cfacfad24f464620
Peter Dimov <pdimov@gmail.com>
2020-09-11 01:12:49 +0300

    Update boost/describe.hpp

--- include/boost/describe.hpp
+++ include/boost/describe.hpp
@@ -6,6 +6,10 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/describe/enumerators.hpp>
+#include <boost/describe/base_classes.hpp>
+#include <boost/describe/data_members.hpp>
 #include <boost/describe/enum.hpp>
+#include <boost/describe/class.hpp>
+#include <boost/describe/modifiers.hpp>
 
 #endif // #ifndef BOOST_DESCRIBE_HPP_INCLUDED
