40fcc4a1ace423ff92b1b953842b0fe2e7fd79ae
Peter Dimov <pdimov@gmail.com>
2020-09-10 16:50:34 +0300

    Add usage examples, some description, some reference

---
+++ example/enum_to_string.cpp
@@ -0,0 +1,32 @@
+#include <boost/describe.hpp>
+#include <boost/mp11.hpp>
+
+template<class E> char const * enum_to_string( E e )
+{
+    char const * r = "(unknown)";
+
+    boost::mp11::mp_for_each< boost::describe::describe_enumerators<E> >([&](auto D){
+
+        if( e == D.value ) r = D.name;
+
+    });
+
+    return r;
+}
+
+#include <iostream>
+
+enum E
+{
+    v1 = 3,
+    v2,
+    v3 = 11
+};
+
+BOOST_DESCRIBE_ENUM(E, v1, v2, v3)
+
+int main()
+{
+    std::cout << "E(" << v1 << "): " << enum_to_string( v1 ) << std::endl;
+    std::cout << "E(" << 0 << "): " << enum_to_string( E(0) ) << std::endl;
+}
