fe26439f394740d7c234a4ae5b06796e9be2c6e5
Peter Dimov <pdimov@gmail.com>
2021-03-13 21:45:24 +0200

    Update bases_test.cpp

--- test/bases_test.cpp
+++ test/bases_test.cpp
@@ -25,15 +25,26 @@ struct X3
 {
 };
 
-struct X4
+struct V1
+{
+    V1(int);
+};
+
+struct V2
+{
+    V2(int);
+};
+
+struct V3
+{
+    V3(int);
+};
+
+struct Y: public X1, protected X2, private X3, public virtual V1, protected virtual V2, private virtual V3
 {
 };
 
-struct Y: public X1, private X2, public virtual X3, private virtual X4
-{
-};
-
-BOOST_DESCRIBE_STRUCT(Y, (X1, X2, X3, X4), ())
+BOOST_DESCRIBE_STRUCT(Y, (X1, X2, X3, V1, V2, V3), ())
 
 #if !defined(BOOST_DESCRIBE_CXX14)
 
@@ -58,21 +69,45 @@ int main()
     }
 
     {
+        using L = describe_bases<X, mod_public>;
+
+        BOOST_TEST_EQ( mp_size<L>::value, 0 );
+    }
+
+    {
+        using L = describe_bases<X, mod_protected>;
+
+        BOOST_TEST_EQ( mp_size<L>::value, 0 );
+    }
+
+    {
+        using L = describe_bases<X, mod_private>;
+
+        BOOST_TEST_EQ( mp_size<L>::value, 0 );
+    }
+
+    {
         using L = describe_bases<Y, mod_any_access>;
 
-        BOOST_TEST_EQ( mp_size<L>::value, 4 );
+        BOOST_TEST_EQ( mp_size<L>::value, 6 );
 
         BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 0>::type, X1 );
         BOOST_TEST_EQ( (mp_at_c<L, 0>::modifiers), mod_public );
 
         BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, X2 );
-        BOOST_TEST_EQ( (mp_at_c<L, 1>::modifiers), mod_private );
+        BOOST_TEST_EQ( (mp_at_c<L, 1>::modifiers), mod_protected );
 
         BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 2>::type, X3 );
-        BOOST_TEST_EQ( (mp_at_c<L, 2>::modifiers), mod_public | mod_virtual );
+        BOOST_TEST_EQ( (mp_at_c<L, 2>::modifiers), mod_private );
 
-        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 3>::type, X4 );
-        BOOST_TEST_EQ( (mp_at_c<L, 3>::modifiers), mod_private | mod_virtual );
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 3>::type, V1 );
+        BOOST_TEST_EQ( (mp_at_c<L, 3>::modifiers), mod_public | mod_virtual );
+
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 4>::type, V2 );
+        BOOST_TEST_EQ( (mp_at_c<L, 4>::modifiers), mod_protected | mod_virtual );
+
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 5>::type, V3 );
+        BOOST_TEST_EQ( (mp_at_c<L, 5>::modifiers), mod_private | mod_virtual );
     }
 
     {
@@ -83,8 +118,20 @@ int main()
         BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 0>::type, X1 );
         BOOST_TEST_EQ( (mp_at_c<L, 0>::modifiers), mod_public );
 
-        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, X3 );
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, V1 );
         BOOST_TEST_EQ( (mp_at_c<L, 1>::modifiers), mod_public | mod_virtual );
+    }
+
+    {
+        using L = describe_bases<Y, mod_protected>;
+
+        BOOST_TEST_EQ( mp_size<L>::value, 2 );
+
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 0>::type, X2 );
+        BOOST_TEST_EQ( (mp_at_c<L, 0>::modifiers), mod_protected );
+
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, V2 );
+        BOOST_TEST_EQ( (mp_at_c<L, 1>::modifiers), mod_protected | mod_virtual );
     }
 
     {
@@ -92,17 +139,11 @@ int main()
 
         BOOST_TEST_EQ( mp_size<L>::value, 2 );
 
-        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 0>::type, X2 );
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 0>::type, X3 );
         BOOST_TEST_EQ( (mp_at_c<L, 0>::modifiers), mod_private );
 
-        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, X4 );
+        BOOST_TEST_TRAIT_SAME( typename mp_at_c<L, 1>::type, V3 );
         BOOST_TEST_EQ( (mp_at_c<L, 1>::modifiers), mod_private | mod_virtual );
-    }
-
-    {
-        using L = describe_bases<Y, mod_protected>;
-
-        BOOST_TEST_EQ( mp_size<L>::value, 0 );
     }
 
     return boost::report_errors();
