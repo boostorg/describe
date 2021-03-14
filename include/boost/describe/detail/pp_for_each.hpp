ae5c3c862fdeeddc75cceb1698cdf3d03fc514ff
Peter Dimov <pdimov@gmail.com>
2021-03-14 06:30:01 +0200

    Add detail/pp_utilities.hpp

--- include/boost/describe/detail/pp_for_each.hpp
+++ include/boost/describe/detail/pp_for_each.hpp
@@ -5,7 +5,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#define BOOST_DESCRIBE_PP_EXPAND(x) x
+#include <boost/describe/detail/pp_utilities.hpp>
 
 #define BOOST_DESCRIBE_PP_FOR_EACH_0(F, a)
 #define BOOST_DESCRIBE_PP_FOR_EACH_1(F, a, x) F(a, x)
@@ -61,10 +61,10 @@
 #define BOOST_DESCRIBE_PP_FOR_EACH_51(F, a, x, ...) BOOST_DESCRIBE_PP_EXPAND(F(a, x) BOOST_DESCRIBE_PP_FOR_EACH_50(F, a, __VA_ARGS__))
 #define BOOST_DESCRIBE_PP_FOR_EACH_52(F, a, x, ...) BOOST_DESCRIBE_PP_EXPAND(F(a, x) BOOST_DESCRIBE_PP_FOR_EACH_51(F, a, __VA_ARGS__))
 
-#define BOOST_DESCRIBE_PP_EXTRACT(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, V, ...) V
+#define BOOST_DESCRIBE_PP_FE_EXTRACT(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, V, ...) V
 
 #define BOOST_DESCRIBE_PP_FOR_EACH(F, ...) \
-    BOOST_DESCRIBE_PP_EXPAND(BOOST_DESCRIBE_PP_EXPAND(BOOST_DESCRIBE_PP_EXTRACT(__VA_ARGS__, \
+    BOOST_DESCRIBE_PP_EXPAND(BOOST_DESCRIBE_PP_EXPAND(BOOST_DESCRIBE_PP_FE_EXTRACT(__VA_ARGS__, \
     BOOST_DESCRIBE_PP_FOR_EACH_52, \
     BOOST_DESCRIBE_PP_FOR_EACH_51, \
     BOOST_DESCRIBE_PP_FOR_EACH_50, \
