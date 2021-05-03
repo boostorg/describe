ef93d95e716c2374cf6fd574b8dbb63ca23b06f5
Peter Dimov <pdimov@gmail.com>
2021-05-03 05:38:05 +0300

    Fix capitalization, MSVC warning

--- example/console.cpp
+++ example/console.cpp
@@ -2,6 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#if defined(_MSC_VER)
+# pragma warning(disable: 4702) // unreachable code
+#endif
+
 #include <boost/describe.hpp>
 #include <boost/mp11.hpp>
 #include <boost/json.hpp>
