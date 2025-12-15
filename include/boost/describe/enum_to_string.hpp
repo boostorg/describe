dfd9182bf3a78d2998c4b863e81cc111f1194df7
Peter Dimov <pdimov@gmail.com>
2025-12-15 12:37:20 +0200

    Replace the lambda in enum_to_string with a struct to enable C++14 constexpr. Refs #60. Refs #61.

--- include/boost/describe/enum_to_string.hpp
+++ include/boost/describe/enum_to_string.hpp
@@ -1,7 +1,7 @@
 #ifndef BOOST_DESCRIBE_ENUM_TO_STRING_HPP_INCLUDED
 #define BOOST_DESCRIBE_ENUM_TO_STRING_HPP_INCLUDED
 
-// Copyright 2020, 2021 Peter Dimov
+// Copyright 2020, 2021, 2025 Peter Dimov
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
@@ -22,16 +22,31 @@ namespace boost
 namespace describe
 {
 
+namespace detail
+{
+
+// [&](auto D){ if( e == D.value ) r = D.name; }
+// except constexpr under C++14
+
+template<class E> struct ets_lambda
+{
+    E e;
+    char const** pr;
+
+    template<class D> constexpr void operator()( D d ) const noexcept
+    {
+        if( e == d.value ) *pr = d.name;
+    }
+};
+
+} // namespace detail
+
 template<class E, class De = describe_enumerators<E>>
 constexpr char const* enum_to_string( E e, char const* def ) noexcept
 {
     char const* r = def;
 
-    mp11::mp_for_each<De>([&](auto D){
-
-        if( e == D.value ) r = D.name;
-
-    });
+    mp11::mp_for_each<De>( detail::ets_lambda<E>{ e, &r } );
 
     return r;
 }
